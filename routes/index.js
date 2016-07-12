7b47b2cc381db28a94304fd14fc01343f9350852
ildafon <ildafon@yandex.ru>
2016-07-12 12:14:29 +0300

    routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title_idex: 'Добро пожаловать!',
+    title_users:'Пользователи',
+    users_data_count:'0'
+  });
 });
 
 module.exports = router;
