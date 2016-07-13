8543ded78aeb5edfb5b9bb223b3d68ed4562bf5b
ildafon <ildafon@yandex.ru>
2016-07-13 21:35:50 +0300

    Mongo read

--- app.js
+++ app.js
@@ -5,9 +5,20 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+//Mongo
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/nodetest1');
+
+
+
+
+
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var admin = require('./routes/admin')
+var admin = require('./routes/admin');
+var userlist = require('./routes/userlist');
 
 var app = express();
 
@@ -26,6 +37,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/admin',admin);
+
+
+app.use(function(req,res,next){
+  req.db = db;
+  next();
+});
+
+app.use('/userlist',userlist);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
